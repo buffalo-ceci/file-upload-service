70e51f6f5598a8fb69626bf6c4a9a501c88de368
buffalo-ceci <buffalo@ceci.com.tw>
2025-12-04 17:22:58 +0800

    Increase JSON request body size limit to 10mb

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const upload = multer({ storage });
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Static file serving
 app.use('/files', express.static(uploadsDir));
